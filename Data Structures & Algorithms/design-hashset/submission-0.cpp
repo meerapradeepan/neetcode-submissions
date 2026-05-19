e01d2b0cad228bc44947214fcdb938b40dc594a1
meerapradeepan <meerapradeepan@gmail.com>
2026-05-18 22:34:05 -0400

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.cpp
@@ -0,0 +1,100 @@
+class BST {
+private:
+    struct TreeNode {
+        int key;
+        TreeNode* left;
+        TreeNode* right;
+        TreeNode(int k) : key(k), left(nullptr), right(nullptr) {}
+    };
+
+    TreeNode* insert(TreeNode* root, int key) {
+        if (!root) return new TreeNode(key);
+        if (key < root->key)
+            root->left = insert(root->left, key);
+        else if (key > root->key)
+            root->right = insert(root->right, key);
+        return root;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (!root) return nullptr;
+        if (key < root->key)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->key)
+            root->right = deleteNode(root->right, key);
+        else {
+            if (!root->left) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            }
+            if (!root->right) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            }
+            TreeNode* temp = minValueNode(root->right);
+            root->key = temp->key;
+            root->right = deleteNode(root->right, temp->key);
+        }
+        return root;
+    }
+
+    TreeNode* minValueNode(TreeNode* root) {
+        while (root->left) root = root->left;
+        return root;
+    }
+
+    bool search(TreeNode* root, int key) {
+        if (!root) return false;
+        if (key == root->key) return true;
+        return key < root->key ? search(root->left, key) : search(root->right, key);
+    }
+
+    TreeNode* root;
+
+public:
+    BST() : root(nullptr) {}
+
+    void add(int key) {
+        root = insert(root, key);
+    }
+
+    void remove(int key) {
+        root = deleteNode(root, key);
+    }
+
+    bool contains(int key) {
+        return search(root, key);
+    }
+};
+
+class MyHashSet {
+private:
+    const int size = 10000;
+    vector<BST> buckets;
+
+    int hash(int key) {
+        return key % size;
+    }
+
+public:
+    MyHashSet() : buckets(size) {}
+
+    void add(int key) {
+        int idx = hash(key);
+        if (!contains(key)) {
+            buckets[idx].add(key);
+        }
+    }
+
+    void remove(int key) {
+        int idx = hash(key);
+        buckets[idx].remove(key);
+    }
+
+    bool contains(int key) {
+        int idx = hash(key);
+        return buckets[idx].contains(key);
+    }
+};
